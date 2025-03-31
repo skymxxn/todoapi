13b7fe1c9c737f3de1fef4b6c975d4c963e77957
Ruslan Yakovlev <ryakovlev19@gmail.com>
2025-03-31 22:08:46 +0500

    Added roles to user model

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using Mapster;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Data;
+using TodoApi.Dtos;
+using TodoApi.Entities;
+
+namespace TodoApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController(TodoContext context) : Controller
+{
+    [HttpGet]
+    [Authorize(Roles = "Admin")]
+    public async Task<ActionResult<List<User>>> GetUsers()
+    {
+        var users = await context.Users.ToListAsync();
+        return Ok(users.Adapt<List<User>>());
+    }
+}
--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -51,7 +51,8 @@ public class AuthService(TodoContext context, IConfiguration configuration) : IA
         var claims = new List<Claim>
         {
             new Claim(ClaimTypes.Name, user.Username),
-            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Role, user.Role)
         };
         var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetValue<string>("AppSettings:Token")!));
         
