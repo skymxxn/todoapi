2b42ae3fa7a015610eaf19291a4eb02e7d03e1d3
Ruslan Yakovlev <ryakovlev19@gmail.com>
2025-04-27 20:05:52 +0500

    Added logging to auth service and improved security

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -20,13 +20,16 @@ public class AuthService : IAuthService
     private readonly TodoDbContext _dbContext;
     private readonly IConfiguration _configuration;
     private readonly SmtpOptions _options;
+    private readonly ILogger<AuthService> _logger;
     
-    public AuthService(TodoDbContext dbContext, IConfiguration configuration, IOptions<SmtpOptions> options)
+    public AuthService(TodoDbContext dbContext, IConfiguration configuration, IOptions<SmtpOptions> options, ILogger<AuthService> logger)
     {
         _dbContext = dbContext;
         _configuration = configuration;
         _options = options.Value;
+        _logger = logger;
     }
+    
     /// Register user and return user object
     public async Task<UserResponseDto?> RegisterAsync(UserRegistrationDto request)
     {
@@ -34,6 +37,7 @@ public class AuthService : IAuthService
         if (await _dbContext.Users.AnyAsync(u => u.Username == request.Username
                                                || u.Email == request.Email))
         {
+            _logger.LogWarning("User with username {Username} or email {Email} already exists", request.Username, request.Email);
             return null;
         }
         
@@ -51,6 +55,8 @@ public class AuthService : IAuthService
         // Save user to database
         _dbContext.Users.Add(user);
         await _dbContext.SaveChangesAsync();
+        
+        _logger.LogInformation("User {Username} created successfully", request.Username);
         
         var emailToken = CreateEmailVerificationToken(user);
         await SendVerificationEmailAsync(user.Email, emailToken);
@@ -78,7 +84,7 @@ public class AuthService : IAuthService
             new Claim(ClaimTypes.Email, user.Email)
         };
         
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["AppSettings:EmailToken"]!));
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["AppSettings:EmailVerificationTokenKey"]!));
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
         
         var tokenDescriptor = new JwtSecurityToken(
@@ -96,7 +102,7 @@ public class AuthService : IAuthService
     public async Task<bool> VerifyEmailTokenAsync(string token)
     {
         var tokenHandler = new JwtSecurityTokenHandler();
-        var key = Encoding.UTF8.GetBytes(_configuration["AppSettings:EmailToken"]!);
+        var key = Encoding.UTF8.GetBytes(_configuration["AppSettings:EmailVerificationTokenKey"]!);
 
         try
         {
@@ -111,18 +117,30 @@ public class AuthService : IAuthService
                 ValidateLifetime = true,
                 ClockSkew = TimeSpan.Zero
             }, out SecurityToken validatedToken);
+            
             var userId = principal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            if (userId is null) return false;
+            if (userId is null)
+            {
+                _logger.LogWarning("User with ID {UserId} does not exist", userId);
+                return false;
+            }
             
             var user = await _dbContext.Users.FindAsync(Guid.Parse(userId));
-            if (user is null) return false;
+            if (user is null)
+            {
+                _logger.LogWarning("User with ID {UserId} does not exist", userId);
+                return false;
+            }
             
             user.IsEmailVerified = true;
             await _dbContext.SaveChangesAsync();
+            
+            _logger.LogInformation("User with ID {UserId} email verified successfully", userId);
             return true;
         } 
-        catch
+        catch (Exception ex)
         {
+            _logger.LogError(ex, "Invalid email verification token");
             return false;
         }
     }
@@ -130,24 +148,34 @@ public class AuthService : IAuthService
     /// Send verification email to user
     private async Task SendVerificationEmailAsync(string email, string emailToken)
     {
-        var message = new MimeMessage();
-        message.From.Add(new MailboxAddress("TodoApp Support", _options.Email));
-        message.To.Add( new MailboxAddress(null, email));
-        message.Subject = "Email Verification";
-        var verificationLink = $"http://localhost:5270/api/auth/verify-email?token={emailToken}";
-        message.Body = new TextPart("html")
+        try
         {
-            Text = $"<h1>Email Verification</h1>" +
-                   $"<p>Please click the link below to verify your email:</p>" +
-                   $"<a href=\"{verificationLink}\">Verify Email</a>" +
-                   $"<p>If you did not request this, please ignore this email.</p>"
-        };
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("TodoApp Support", _options.Email));
+            message.To.Add( new MailboxAddress(null, email));
+            message.Subject = "Email Verification";
+            var verificationLink = string.Format(_configuration["Frontend:VerificationUrl"]!, emailToken);
+            message.Body = new TextPart("html")
+            {
+                Text = $"<h1>Email Verification</h1>" +
+                       $"<p>Please click the link below to verify your email:</p>" +
+                       $"<a href=\"{verificationLink}\">Verify Email</a>" +
+                       $"<p>If you did not request this, please ignore this email.</p>"
+            };
         
-        using var client = new MailKit.Net.Smtp.SmtpClient();
-        await client.ConnectAsync(_options.Host, _options.Port, MailKit.Security.SecureSocketOptions.StartTls);
-        await client.AuthenticateAsync(_options.Email, _options.Password);
-        await client.SendAsync(message);
-        await client.DisconnectAsync(true);
+            using var client = new MailKit.Net.Smtp.SmtpClient();
+            await client.ConnectAsync(_options.Host, _options.Port, MailKit.Security.SecureSocketOptions.StartTls);
+            await client.AuthenticateAsync(_options.Email, _options.Password);
+            await client.SendAsync(message);
+            await client.DisconnectAsync(true);
+        
+            _logger.LogInformation("Verification email sent to {Email}", email);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error sending verification email");
+            throw;
+        }
     }
     
     /// Login user and return access and refresh tokens
@@ -157,13 +185,18 @@ public class AuthService : IAuthService
         // Check if user exists and if password is correct
         if (user == null || !BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
         {
+            _logger.LogWarning("Invalid login attempt for user with email {Email}", request.Email);
             return null;
         }
         
         if (!user.IsEmailVerified)
         {
+            _logger.LogWarning("User with email {Email} has not verified their email", request.Email);
+            
             var emailToken = CreateEmailVerificationToken(user);
             await SendVerificationEmailAsync(user.Email, emailToken);
+            
+            _logger.LogInformation("Verification email sent to {Email}", request.Email);
             return null;
         }
        
@@ -172,6 +205,7 @@ public class AuthService : IAuthService
         _dbContext.Users.Update(user);
         await _dbContext.SaveChangesAsync();
         
+        _logger.LogInformation("User with email {Email} logged in successfully", request.Email);
         return await CreateTokenResponse(user);
     }
 
@@ -180,14 +214,15 @@ public class AuthService : IAuthService
     {
         var response = new TokenResponseDto
         {
-            AccessToken = CreateToken(user),
+            AccessToken = CreateAccessToken(user),
             RefreshToken = await GenerateAndSaveRefreshTokenAsync(user)
         };
+        
         return response;
     }
 
     /// Create JWT token
-    private string CreateToken(User user)
+    private string CreateAccessToken(User user)
     {
         // Create claims
         var claims = new List<Claim>
@@ -196,7 +231,7 @@ public class AuthService : IAuthService
             new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
             new Claim(ClaimTypes.Role, user.Role)
         };
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetValue<string>("AppSettings:Token")!));
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetValue<string>("AppSettings:AccessTokenKey")!));
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
         
         var tokenDescriptor = new JwtSecurityToken(
@@ -216,8 +251,10 @@ public class AuthService : IAuthService
         var user = await ValidateRefreshTokenAsync(request.UserId, request.RefreshToken);
         if (user is null)
         {
+            _logger.LogError("Invalid refresh token for user {UserId}", request.UserId);
             return null;
         }
+        
         return await CreateTokenResponse(user);
     }
     
@@ -228,6 +265,7 @@ public class AuthService : IAuthService
         if (user == null || user.RefreshToken != refreshToken 
                          || user.RefreshTokenExpiryTime < DateTime.UtcNow)
         {
+            _logger.LogError("Invalid refresh token for user {UserId}", userId);
             return null;
         }
         
@@ -237,9 +275,11 @@ public class AuthService : IAuthService
     /// Generate a new refresh token
     private string GenerateRefreshToken()
     {
+        
         var randomNumber = new byte[32];
         using var rng = RandomNumberGenerator.Create();
         rng.GetBytes(randomNumber);
+        
         return Convert.ToBase64String(randomNumber);
     }
 
@@ -250,6 +290,7 @@ public class AuthService : IAuthService
         user.RefreshToken = refreshToken;
         user.RefreshTokenExpiryTime = DateTime.UtcNow.AddDays(7);
         await _dbContext.SaveChangesAsync();
+        
         return refreshToken;
     }
 
@@ -257,15 +298,23 @@ public class AuthService : IAuthService
     public async Task<bool> ChangePasswordAsync(Guid userId,ChangePasswordDto request)
     {
         var user = await _dbContext.Users.FindAsync(userId);
-        if (user is null) return false;
+        if (user is null)
+        {
+            _logger.LogWarning("User with ID {UserId} does not exist", userId);
+            return false;
+        }
         
         if (!BCrypt.Net.BCrypt.Verify(request.OldPassword, user.PasswordHash))
         {
+            _logger.LogWarning("Invalid old password for user with ID {UserId}", userId);
             return false;
         }
+        
         user.PasswordHash = BCrypt.Net.BCrypt.HashPassword(request.NewPassword);
         _dbContext.Users.Update(user);
         await _dbContext.SaveChangesAsync();
+        
+        _logger.LogInformation("Password changed successfully for user with ID {UserId}", userId);
         return true;
     }
 
@@ -273,10 +322,16 @@ public class AuthService : IAuthService
     public async Task<bool> RequestPasswordResetAsync(string email)
     {
         var user = await _dbContext.Users.FirstOrDefaultAsync(u => u.Email == email);
-        if (user is null || !user.IsEmailVerified) return false;
+        if (user is null || !user.IsEmailVerified)
+        {
+            _logger.LogWarning("User with email {Email} does not exist or has not verified their email", email);
+            return false;
+        }
         
         var token = CreatePasswordResetToken(user);
         await SendPasswordResetEmailAsync(user.Email, token);
+        
+        _logger.LogInformation("Password reset email sent to {Email}", email);
         return true;
     }
 
@@ -289,7 +344,7 @@ public class AuthService : IAuthService
             new Claim(ClaimTypes.Email, user.Email)
         };
         
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["AppSettings:EmailToken"]!));
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["AppSettings:PasswordResetTokenKey"]!));
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
         
         var tokenDescriptor = new JwtSecurityToken(
@@ -306,31 +361,41 @@ public class AuthService : IAuthService
     /// Send password reset email to user
     private async Task SendPasswordResetEmailAsync(string email, string token)
     {
-        var message = new MimeMessage();
-        message.From.Add(new MailboxAddress("TodoApp Support", _options.Email));
-        message.To.Add( new MailboxAddress(null, email));
-        message.Subject = "Password Reset";
-        var resetLink = $"http://localhost:5270/api/auth/reset-password?token={token}";
-        message.Body = new TextPart("html")
+        try
         {
-            Text = $"<h1>Password Reset</h1>" +
-                   $"<p>Please click the link below to reset your password:</p>" +
-                   $"<a href=\"{resetLink}\">Reset Password</a>" +
-                   $"<p>If you did not request this, please ignore this email.</p>"
-        };
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("TodoApp Support", _options.Email));
+            message.To.Add( new MailboxAddress(null, email));
+            message.Subject = "Password Reset";
+            var resetLink = string.Format(_configuration["Frontend:VerificationUrl"]!, token);
+            message.Body = new TextPart("html")
+            {
+                Text = $"<h1>Password Reset</h1>" +
+                       $"<p>Please click the link below to reset your password:</p>" +
+                       $"<a href=\"{resetLink}\">Reset Password</a>" +
+                       $"<p>If you did not request this, please ignore this email.</p>"
+            };
         
-        using var client = new MailKit.Net.Smtp.SmtpClient();
-        await client.ConnectAsync(_options.Host, _options.Port, MailKit.Security.SecureSocketOptions.StartTls);
-        await client.AuthenticateAsync(_options.Email, _options.Password);
-        await client.SendAsync(message);
-        await client.DisconnectAsync(true);
+            using var client = new MailKit.Net.Smtp.SmtpClient();
+            await client.ConnectAsync(_options.Host, _options.Port, MailKit.Security.SecureSocketOptions.StartTls);
+            await client.AuthenticateAsync(_options.Email, _options.Password);
+            await client.SendAsync(message);
+            await client.DisconnectAsync(true);
+        
+            _logger.LogInformation("Password reset email sent to {Email}", email);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error sending password reset email to {Email}", email);
+            throw;
+        }
     }
 
     /// Reset user password using token
     public async Task<bool> ResetPasswordAsync(string token, string newPassword)
     {
         var tokenHandler = new JwtSecurityTokenHandler();
-        var key = Encoding.UTF8.GetBytes(_configuration["AppSettings:EmailToken"]!);
+        var key = Encoding.UTF8.GetBytes(_configuration["AppSettings:PasswordResetTokenKey"]!);
 
         try
         {
@@ -347,17 +412,28 @@ public class AuthService : IAuthService
             }, out SecurityToken validatedToken);
             
             var userId = principal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            if (userId is null) return false;
+            if (userId is null)
+            {
+                _logger.LogWarning("User with ID {UserId} does not exist", userId);
+                return false;
+            }
             
             var user = await _dbContext.Users.FindAsync(Guid.Parse(userId));
-            if (user is null) return false;
+            if (user is null)
+            {
+                _logger.LogWarning("User with ID {UserId} does not exist", userId);
+                return false;
+            }
             
             user.PasswordHash = BCrypt.Net.BCrypt.HashPassword(newPassword);
             await _dbContext.SaveChangesAsync();
+            
+            _logger.LogInformation("Password reset successfully for user with ID {UserId}", userId);
             return true;
         }
-        catch
+        catch (Exception ex)
         {
+            _logger.LogError(ex, "Invalid password reset token");
             return false;
         }
     }
@@ -366,12 +442,18 @@ public class AuthService : IAuthService
     public async Task<bool> LogoutAsync(Guid userId)
     {
         var user = await  _dbContext.Users.FindAsync(userId);
-        if (user is null) return false;
+        if (user is null)
+        {
+            _logger.LogWarning("User with ID {UserId} does not exist", userId);
+            return false;
+        }
         
         user.RefreshToken = null;
         user.RefreshTokenExpiryTime = null;
         
         await _dbContext.SaveChangesAsync();
+        
+        _logger.LogInformation("User with ID {UserId} logged out successfully", userId);
         return true;
     }
 }
